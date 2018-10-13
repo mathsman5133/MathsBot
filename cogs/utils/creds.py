6edc0e1d6e3f93b4fe998a838bbe74b4eedfb892
mathsman5133 <mathsman5132@gmail.com>
2018-10-13 18:20:30 +1100

    fix .gitignore

--- cogs/utils/creds.py
+++
@@ -1,3 +0,0 @@
-discordtoken = 'NDk2NTU4NTcxNjA1OTgzMjYy.DpSX5g.FeDrJG5g3YJ76Jp_T0VUysmcAvY'
-webhookid = 497234452309737475
-webhooktoken = '7xy_Z1-3JYLCXBOYPX1EpICdKc6bdG8UkRigb1tjS1NFSKBkEv1lNQjpV3aY3-JimB18'
