eae8dce007987a462021e9794dcb8351038b490a
mathsman5133 <mathsman5132@gmail.com>
2018-11-27 19:46:39 +1100

    new database

--- cogs/jokes.py
+++ cogs/jokes.py
@@ -5,8 +5,51 @@ import discord
 import asyncio
 from cogs.games import Leaderboard
 import os
+from cogs.utils import db
 
 db_path = os.path.join(os.getcwd(), 'cogs', 'utils', 'database.db')
+
+
+class AllJokes(db.Table, table_name='jokes'):
+    id = db.PrimaryKeyColumn()
+
+    category = db.Column(db.String)
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+
+
+class DadJokes(db.Table):
+    id = db.PrimaryKeyColumn()
+
+    joke = db.Column(db.String)
+    unique = db.Column(db.String)
+
+
+class SavedJokes(db.Table):
+    id = db.PrimaryKeyColumn()
+
+    category = db.Column(db.String)
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+    joke_id = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+
+
+class Riddles(db.Table):
+    id = db.PrimaryKeyColumn()
+
+    riddle = db.Column(db.String)
+    answer = db.Column(db.String)
+    used = db.Column(db.Integer, default=0)
+
+
+class SavedRiddles(db.Table):
+    id = db.PrimaryKeyColumn()
+
+    userid = db.Column(db.Integer(big=True))
+    riddle_number = db.Column(db.Integer)
+    question = db.Column(db.String)
+    answer = db.Column(db.String)
 
 
 class Jokes:
@@ -20,17 +63,22 @@ class Jokes:
             EXAMPLE: `dadjoke` or `dadjoke 432`
             RESULT: Shows a random dad-joke or the joke with id `432`"""
         # if no joke id
-        if not jokeid:
-            # random number b/w 1 and 334 (number of dadjokes) obv before I knew sql rand func
-            number = random.randint(1, 344)
-            async with aiosqlite.connect(db_path) as db:
-                c = await db.execute("SELECT * FROM dadjokes WHERE `unique` = :id", {'id': number})
-                dump = await c.fetchall()
-        # otherwise get joke with specific id
+        if jokeid:
+            query = "SELECT * FROM dadjokes WHERE `id` = $1"
+            dump = await self.bot.pool.fetch(query, jokeid)
+
+            # async with aiosqlite.connect(db_path) as db:
+            #     c = await db.execute("SELECT * FROM dadjokes WHERE `unique` = :id", {'id': number})
+            #     dump = await c.fetchall()
+
         else:
-            async with aiosqlite.connect(db_path) as db:
-                c = await db.execute("SELECT * FROM dadjokes WHERE `id` = :id", {'id': jokeid})
-                dump = await c.fetchall()
+            query = "SELECT * FROM dadjokes ORDER BY RANDOM() LIMIT 1"
+            dump = await self.bot.pool.fetch(query)
+
+            # async with aiosqlite.connect(db_path) as db:
+            #     c = await db.execute("SELECT * FROM dadjokes WHERE `id` = :id", {'id': jokeid})
+            #     dump = await c.fetchall()
+
         embed = discord.Embed(colour=0x00ffff)
         embed.set_author(name=dump[0][0], icon_url=ctx.author.avatar_url)
         embed.set_footer(text=f"Like that dad joke? The ID was: {dump[0][1]}. Powered by https://icanhazdadjoke.com/")
@@ -42,42 +90,51 @@ class Jokes:
             PARAMETERS: optional: [category] - a joke with that category
             EXAMPLE: `joke` or `joke Knock Knock`
             RESULT: Random joke or random Knock Knock joke"""
-        # if no category get random joke
+
         if not category:
-            async with aiosqlite.connect(db_path) as db:
-                c = await db.execute("SELECT * FROM jokes ORDER BY RANDOM() LIMIT 1")
-                dump = await c.fetchall()
-        # else get joke with category
+            query = "SELECT * FROM jokes ORDER BY RANDOM() LIMIT 1"
+            dump = await self.bot.pool.fetch(query)
+
+            # async with aiosqlite.connect(db_path) as db:
+            #     c = await db.execute("SELECT * FROM jokes ORDER BY RANDOM() LIMIT 1")
+            #     dump = await c.fetchall()
+
         else:
             # if they want to know available categories
             if category == 'category':
-                async with aiosqlite.connect(db_path) as db:
-                    c = await db.execute("SELECT category FROM jokes")
-                    dump = list(set(await c.fetchall()))
-                    embed = discord.Embed(colour=0x00ff00)
-                    embed.set_author(name="Categories for the joke commands", icon_url=ctx.author.avatar_url)
-                    desc = ''
-                    for cat in dump:
-                        desc += cat[0] + '\n'
-                    embed.add_field(name='\u200b', value=desc)
-                    embed.set_footer(text="Type `.joke [category]` to get a joke of that category!",
-                                     icon_url=self.bot.user.avatar_url)
-                    await ctx.send(embed=embed)
-                    return
-            # otherwise get joke with category
-            async with aiosqlite.connect(db_path) as db:
-                c = await db.execute("SELECT * FROM jokes WHERE category = :cat ORDER BY RANDOM() LIMIT 1",
-                                     {'cat': category})
-                dump = await c.fetchall()
+                query = "SELECT category FROM jokes"
+                dump = list(set(await self.bot.pool.fetch(query)))
 
-        # category not found in db (returned nothing)
+                desc = '{}\n'.join(n[0] for n in dump)
+
+                #
+                # async with aiosqlite.connect(db_path) as db:
+                #     c = await db.execute("SELECT category FROM jokes")
+                #     dump = list(set(await c.fetchall()))
+
+                embed = discord.Embed(colour=0x00ff00)
+
+                embed.set_author(name="Categories for the joke commands", icon_url=ctx.author.avatar_url)
+                embed.add_field(name='\u200b', value=desc)
+                embed.set_footer(text="Type `.joke [category]` to get a joke of that category!",
+                                 icon_url=self.bot.user.avatar_url)
+
+                return await ctx.send(embed=embed)
+
+            query = "SELECT * FROM jokes WHERE category = $1 ORDER BY RANDOM() LIMIT 1"
+            dump = await self.bot.pool.fetch(query, category)
+
+            # async with aiosqlite.connect(db_path) as db:
+            #     c = await db.execute("SELECT * FROM jokes WHERE category = :cat ORDER BY RANDOM() LIMIT 1",
+            #                          {'cat': category})
+            #     dump = await c.fetchall()
 
         if len(dump) == 0:
             embed = discord.Embed(colour=0xff0000)
             embed.set_author(name="Category not found", icon_url=ctx.author.avatar_url)
             embed.description = 'Find all categories with .joke categories command'
-            await ctx.send(embed=embed)
-            return
+            return await ctx.send(embed=embed)
+
         # otherwise post joke
         embed = discord.Embed(colour=0x00ffff)
         embed.set_author(name=f"{dump[0][2]}", icon_url=ctx.author.avatar_url)
@@ -88,31 +145,32 @@ class Jokes:
         msg = await ctx.send(embed=embed)
 
         # idk why this is here since jokes are that shitty all over anyways
-        def check(reaction, user):
-            # check if reacter is author and emoji is star
-            return user.id != self.bot.user.id and str(reaction.emoji) == '\N{WHITE MEDIUM STAR}'
-        await msg.add_reaction('\N{WHITE MEDIUM STAR}')
-        score = 0
-        while True:
-            try:
-                # 10min timeout to get a like for the joke
-                reaction = await self.bot.wait_for('reaction_add', check=check, timeout=600)
-                score += 1
-                print(score)
-            except asyncio.TimeoutError:
-                # when timeout if it has been liked
-                if score > 0:
-                    # i dont have a command to get this yet but w/e
-                    async with aiosqlite.connect(db_path) as db:
-                        await db.execute("INSERT INTO savedjokes VALUES (Null, :cat, :tit, :body, :jokeid, :score)",
-                                         {'cat': dump[0][1], 'tit': dump[0][2], 'body': dump[0][3], 'jokeid': dump[0][0],
-                                          'score': score})
-                        await db.commit()
-                    embed = discord.Embed(colour=0x00ff00)
-                    embed.set_author(name=f"Joke ID {dump[0][0]} was saved with {score} votes!",
-                                     icon_url=self.bot.avatar_url)
-                    await ctx.send(embed=embed)
-                    break
+        # def check(reaction, user):
+        #     # check if reacter is author and emoji is star
+        #     return user.id != self.bot.user.id and str(reaction.emoji) == '\N{WHITE MEDIUM STAR}'
+        # await msg.add_reaction('\N{WHITE MEDIUM STAR}')
+        #
+        # score = 0
+        # while True:
+        #     try:
+        #         # 10min timeout to get a like for the joke
+        #         reaction = await self.bot.wait_for('reaction_add', check=check, timeout=600)
+        #         score += 1
+        #
+        #     except asyncio.TimeoutError:
+        #         # when timeout if it has been liked
+        #         if score > 0:
+        #             # i dont have a command to get this yet but w/e
+        #             async with aiosqlite.connect(db_path) as db:
+        #                 await db.execute("INSERT INTO savedjokes VALUES (Null, :cat, :tit, :body, :jokeid, :score)",
+        #                                  {'cat': dump[0][1], 'tit': dump[0][2], 'body': dump[0][3], 'jokeid': dump[0][0],
+        #                                   'score': score})
+        #                 await db.commit()
+        #             embed = discord.Embed(colour=0x00ff00)
+        #             embed.set_author(name=f"Joke ID {dump[0][0]} was saved with {score} votes!",
+        #                              icon_url=self.bot.avatar_url)
+        #             await ctx.send(embed=embed)
+        #             break
 
     @commands.command()
     async def insult(self, ctx, user: discord.Member = None):
@@ -123,11 +181,17 @@ class Jokes:
         # if they didnt ping someone I will insult them
         if not user:
             user = ctx.author
-        # get random joke with category 'insult'
-        async with aiosqlite.connect(db_path) as db:
-            c = await db.execute("SELECT * FROM jokes WHERE category = 'Insults' "
-                                 "AND title = 'Stupid Stuff' ORDER BY RANDOM() LIMIT 1")
-            dump = await c.fetchall()
+
+        query = """
+                SELECT * FROM jokes WHERE category = 'Insults'
+                AND title = 'Stupid Stuff' ORDER BY RANDOM() LIMIT 1
+                """
+        dump = await self.bot.pool.fetch(query)
+        #
+        # async with aiosqlite.connect(db_path) as db:
+        #     c = await db.execute("SELECT * FROM jokes WHERE category = 'Insults' "
+        #                          "AND title = 'Stupid Stuff' ORDER BY RANDOM() LIMIT 1")
+        #     dump = await c.fetchall()
         embed = discord.Embed(colour=0x00ffff)
         embed.set_author(name=f'{user.display_name}#{user.discriminator}, {dump[0][3]}', icon_url=ctx.author.avatar_url)
         embed.set_thumbnail(url=user.avatar_url)
@@ -135,30 +199,30 @@ class Jokes:
                               f"Like this insult? React with :star: emoji in next 10min to add it to liked jokes!",
                          icon_url=self.bot.user.avatar_url)
         msg = await ctx.send(embed=embed)
-        await msg.add_reaction('\N{WHITE MEDIUM STAR}')
 
-        #same as joke. insults are pretty trash tbh too
-        def check(reaction, user):
-            return user.id != self.bot.user.id and str(reaction.emoji) == '\N{WHITE MEDIUM STAR}'
-
-        score = 0
-        while True:
-            try:
-                reaction = await self.bot.wait_for('reaction_add', check=check, timeout=600)
-                score += 1
-                print(score)
-            except asyncio.TimeoutError:
-                if score > 0:
-                    async with aiosqlite.connect(db_path) as db:
-                        await db.execute("INSERT INTO savedjokes VALUES (Null, :cat, :tit, :body, :jokeid, :score)",
-                                         {'cat': dump[0][1], 'tit': dump[0][2], 'body': dump[0][3], 'jokeid': dump[0][0],
-                                          'score': score})
-                        await db.commit()
-                    embed = discord.Embed(colour=0x00ff00)
-                    embed.set_author(name=f"Insult ID {dump[0][0]} was saved with {score} votes!",
-                                     icon_url=ctx.author.avatar_url)
-                    await ctx.send(embed=embed)
-                    break
+        # await msg.add_reaction('\N{WHITE MEDIUM STAR}')
+        #
+        # def check(reaction, user):
+        #     return user.id != self.bot.user.id and str(reaction.emoji) == '\N{WHITE MEDIUM STAR}'
+        #
+        # score = 0
+        # while True:
+        #     try:
+        #         reaction = await self.bot.wait_for('reaction_add', check=check, timeout=600)
+        #         score += 1
+        #         print(score)
+        #     except asyncio.TimeoutError:
+        #         if score > 0:
+        #             async with aiosqlite.connect(db_path) as db:
+        #                 await db.execute("INSERT INTO savedjokes VALUES (Null, :cat, :tit, :body, :jokeid, :score)",
+        #                                  {'cat': dump[0][1], 'tit': dump[0][2], 'body': dump[0][3], 'jokeid': dump[0][0],
+        #                                   'score': score})
+        #                 await db.commit()
+        #             embed = discord.Embed(colour=0x00ff00)
+        #             embed.set_author(name=f"Insult ID {dump[0][0]} was saved with {score} votes!",
+        #                              icon_url=ctx.author.avatar_url)
+        #             await ctx.send(embed=embed)
+        #             break
 
     @commands.command()
     async def riddle(self, ctx, number: int=None):
@@ -172,9 +236,12 @@ class Jokes:
             number = random.randint(1, 1478)
         else:
             number = number
-        async with aiosqlite.connect(db_path) as db:
-            c = await db.execute("SELECT * FROM riddles WHERE `unique`=:uni", {'uni':number})
-            riddle = await c.fetchall()
+        query = "SELECT * FROM riddles WHERE `unique`=$1"
+        riddle = await self.bot.pool.fetch(query, number)
+
+        # async with aiosqlite.connect(db_path) as db:
+        #     c = await db.execute("SELECT * FROM riddles WHERE `unique`=:uni", {'uni':number})
+        #     riddle = await c.fetchall()
 
         # check if reply is author
         def check(user):
@@ -182,6 +249,7 @@ class Jokes:
                 return False
             else:
                 return True
+
         # send the riddle
         lb = Leaderboard(ctx)
         embed = discord.Embed(colour=0x00ffff)
